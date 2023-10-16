82362187231fcf9223674c2b21f6cdbb5ef73b08
BuildTools <unconfigured@null.spigotmc.org>
2023-10-16 15:19:05 -0300

    FIX pixel overflow

--- facilita_project/lib/pages/login_page.dart
+++ facilita_project/lib/pages/login_page.dart
@@ -16,114 +16,117 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 20, 39, 22),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //logo
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+                //logo
 
-              Image.asset(
-                'lib/images/corvo.png',
-                height: 100,
-              ),
-
-              const SizedBox(height: 50),
-
-              //welcome message
-              const Text(
-                'Bem-vindo à Facilita!',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
+                Image.asset(
+                  'lib/images/corvo.png',
+                  height: 100,
                 ),
-              ),
 
-              const SizedBox(height: 25),
+                const SizedBox(height: 50),
 
-              //username
-              MyTextFields(
-                controller: usernameController,
-                hintText: 'Usuário',
-                obscureText: false,
-              ),
+                //welcome message
+                const Text(
+                  'Bem-vindo à Facilita!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 25),
 
-              //password
-              MyTextFields(
-                controller: passwordController,
-                hintText: 'Senha',
-                obscureText: true,
-              ),
+                //username
+                MyTextFields(
+                  controller: usernameController,
+                  hintText: 'Usuário',
+                  obscureText: false,
+                ),
 
-              const SizedBox(height: 10),
+                const SizedBox(height: 10),
 
-              //  forgot password
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+                //password
+                MyTextFields(
+                  controller: passwordController,
+                  hintText: 'Senha',
+                  obscureText: true,
+                ),
+
+                const SizedBox(height: 10),
+
+                //  forgot password
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text('Esqueceu sua senha?',
+                          style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                //  login button
+                MyButton(
+                  onTap: loginUserIn,
+                ),
+
+                const SizedBox(height: 50),
+
+                //  or continue with
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'ou continue com',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                //  google + apple login
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Esqueceu sua senha?',
-                        style: TextStyle(color: Colors.white)),
+                    //  google button
+                    SquareTile(imagePath: 'lib/images/google-icon.png'),
+
+                    SizedBox(width: 25),
+
+                    //  apple button
+                    SquareTile(imagePath: 'lib/images/apple-icon.png'),
                   ],
                 ),
-              ),
-
-              const SizedBox(height: 50),
-
-              //  login button
-              MyButton(
-                onTap: loginUserIn,
-              ),
-
-              const SizedBox(height: 50),
-
-              //  or continue with
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'ou continue com',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              //  google + apple login
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  //  google button
-                  SquareTile(imagePath: 'lib/images/google-icon.png'),
-
-                  SizedBox(width: 25),
-
-                  //  apple button
-                  SquareTile(imagePath: 'lib/images/apple-icon.png'),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
