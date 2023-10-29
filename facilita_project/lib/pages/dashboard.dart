419f5f11be2b062709a6bc1b756375f223d49a06
BuildTools <unconfigured@null.spigotmc.org>
2023-10-29 18:52:18 -0300

    ADD carteira_container

--- facilita_project/lib/pages/dashboard.dart
+++ facilita_project/lib/pages/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:facilita_project/components/carteira_container.dart';
 import 'package:flutter/material.dart';
 import 'package:facilita_project/components/select_button.dart';
 
@@ -36,68 +37,9 @@ class Dashboard extends StatelessWidget {
                 ),
                 //  saldo
                 const SizedBox(height: 10),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20,
-                    vertical: 10,
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color: const Color.fromARGB(255, 25, 43, 27),
-                          ),
-                          height: 175,
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                alignment: Alignment
-                                    .center, // Center the "CARTEIRA" text
-                                child: const Text(
-                                  "CARTEIRA",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                              const Text(
-                                "Saldo: 100",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Text(
-                                "Despesas: 500",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const Text(
-                                "Despesas: 500",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+
+                //  CARTEIRA CONTAINER
+                const CarteiraContainer(),
               ],
             ),
           ),
