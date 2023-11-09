6d96afce1728073c43a8e50c96f01ac2961cd920
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 09:36:10 -0300

    ADD date_container.dart

---
+++ facilita_project/lib/components/date_container.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DateContainer extends StatelessWidget {
+  const DateContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: const Color.fromARGB(255, 6, 20, 18),
+              ),
+              height: 175,
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    // Removed 'child' property here
+                    child: const Text(
+                      "Apartir de quando?",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Row(
+                    children: [
+                      Icon(Icons.hourglass_empty, color: Colors.white),
+                      SizedBox(width: 10), // Add some spacing
+                      Text(
+                        "Início: 10/10/2023",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Row(
+                    children: [
+                      Icon(Icons.hourglass_full, color: Colors.white),
+                      SizedBox(width: 10), // Add some spacing
+                      Text(
+                        "Fim: 15/11/2023",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ facilita_project/lib/pages/extrato.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:facilita_project/components/date_container.dart';
+
+class Extrato extends StatelessWidget {
+  const Extrato({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 5, 29, 31),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              //  icons to use:
+              //  1 airplanemode_active_outlined
+              //  2 fastfood
+              //  3 shopping_cart_rounded
+              //  4 directions_car
+              //  5 monetization_on
+              //  6 mobile_friendly
+              //  7 people_alt
+              //  8 wb_incandescent
+              //  9 house
+              //  10 water_drop
+              //  11 web
+              //  12 credit_card
+              children: [
+                DateContainer(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
