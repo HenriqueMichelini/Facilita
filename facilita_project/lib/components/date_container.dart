22b1457221242462cdc3990e726561d83b19ac89
BuildTools <unconfigured@null.spigotmc.org>
2023-12-05 10:14:08 -0300

    FIX extrato.dart dates

--- facilita_project/lib/components/date_container.dart
+++ facilita_project/lib/components/date_container.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 
-class DateContainer extends StatelessWidget {
-  const DateContainer({super.key});
+class DateContainer extends StatefulWidget {
+  const DateContainer({Key? key}) : super(key: key);
+
+  @override
+  _DateContainerState createState() => _DateContainerState();
+}
+
+class _DateContainerState extends State<DateContainer> {
+  late DateTime _selectedDate;
+  late DateTime _startDate;
+  late DateTime _endDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDate = DateTime.now();
+    _startDate = DateTime(2000, 01, 01);
+    _endDate = DateTime.now(); // Set _endDate to current system date
+  }
+
+  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: isStartDate ? _startDate : _endDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != (isStartDate ? _startDate : _endDate)) {
+      setState(() {
+        if (isStartDate) {
+          _startDate = picked;
+        } else {
+          _endDate = picked;
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +61,6 @@ class DateContainer extends StatelessWidget {
                 children: [
                   Container(
                     alignment: Alignment.center,
-                    // Removed 'child' property here
                     child: const Text(
                       "Apartir de quando?",
                       style: TextStyle(
@@ -36,30 +70,36 @@ class DateContainer extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const Row(
+                  Row(
                     children: [
                       Icon(Icons.hourglass_empty, color: Colors.white),
-                      SizedBox(width: 10), // Add some spacing
-                      Text(
-                        "Início: 10/10/2023",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      SizedBox(width: 10),
+                      TextButton(
+                        onPressed: () => _selectDate(context, true),
+                        child: Text(
+                          "Início: ${_startDate.day}/${_startDate.month}/${_startDate.year}",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ],
                   ),
-                  const Row(
+                  Row(
                     children: [
                       Icon(Icons.hourglass_full, color: Colors.white),
-                      SizedBox(width: 10), // Add some spacing
-                      Text(
-                        "Fim: 15/11/2023",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                      SizedBox(width: 10),
+                      TextButton(
+                        onPressed: () => _selectDate(context, false),
+                        child: Text(
+                          "Fim: ${_endDate.day}/${_endDate.month}/${_endDate.year}",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ],
