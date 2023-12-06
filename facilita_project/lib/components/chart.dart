0efd19befc45bf783a2957697efcb3b5dc7366f2
Diego Barbosa <diego1279.dr@gmail.com>
2023-12-06 17:03:46 -0300

    totalizador grafico

--- facilita_project/lib/components/chart.dart
+++ facilita_project/lib/components/chart.dart
@@ -1,14 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:facilita_project/data/models/financial_model.dart';
+import 'package:facilita_project/data/repositories/financial_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
+import 'package:rxdart/rxdart.dart';
 
-class Chart extends StatelessWidget {
-  const Chart({super.key});
+class Chart extends StatefulWidget {
+  const Chart({
+    super.key,
+  });
+
+  @override
+  State<Chart> createState() => _ChartState();
+}
+
+class _ChartState extends State<Chart> {
+  final financial = BehaviorSubject<FinancialModel>();
+  final FinancialRepository repository = FinancialRepository(dio: Dio());
+  late FinancialModel totalazer;
+
+  @override
+  void initState() {
+    super.initState();
+    totalazer = getFinancialTotalizer();
+  }
+
+  getFinancialTotalizer() async {
+    var result = await repository.getTotalizer();
+    financial.add(result);
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
     final dataMap = <String, double>{
-      "Saldo": 6000,
-      "Despesas": 4000,
+      "Depósito": 25,
+      "Despesas": 32,
     };
 
     final colorList = <Color>[
